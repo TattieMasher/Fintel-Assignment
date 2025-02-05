2326152cea87eab5f334f4a257964ac35fc6326c
TattieMasher <80898758+TattieMasher@users.noreply.github.com>
2025-02-05 20:31:49 +0000

    Different task card layout. Looks better, but still rubbish with large text content.

--- todo/resources/views/livewire/task-card.blade.php
+++ todo/resources/views/livewire/task-card.blade.php
@@ -1,14 +1,16 @@
 <div class="task-card {{ $task['completed'] ? 'completed' : '' }}">
-    <div class="flex justify-between">
-        <h4>{{ $task['title'] }}</h4>
-        <p>{{ $task['description'] }}</p>
+    <div class="flex">
         <h4>{{ $task['priority'] }}</h4>
-    </div>
+        <div class="flex-col justify-between">
+            <h4>{{ $task['title'] }}</h4>
+            <p>{{ $task['description'] }}</p>
+        </div>
 
-    <div class="flex justify-between">
-        <button wire:click="toggleTask" class="primary">
-            {{ $task['completed'] ? 'Mark Incomplete' : 'Mark Complete' }}
-        </button>
-        <button wire:click="deleteTask" class="error">Delete</button>
+        <div class="flex justify-between">
+            <button wire:click="toggleTask" class="primary">
+                {{ $task['completed'] ? 'Mark Incomplete' : 'Mark Complete' }}
+            </button>
+            <button wire:click="deleteTask" class="error">Delete</button>
+        </div>
     </div>
 </div>
